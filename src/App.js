c2df2c3d5537164ebaaf0a73c4809e5a7d11a34d
palakurthiprashanth <palakurthi.prashanth1@gmail.com>
2021-10-20 18:49:42 +0530

    DynamicLoading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Home from './PortfolioContainer/Home/Home';
+import PortfolioContainer from './PortfolioContainer/PortfolioContainer';
 
 import './app.css';
 
 export default function App() {
   return (
     <div className="App">
-      <Home />
+      <PortfolioContainer />
     </div>
   );
 }
